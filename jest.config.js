5f2e4c8c8dd7469924f093568241a57361be357f
Shubham Singh <imshubhamsingh97@gmail.com>
2019-07-31 21:07:24 +0530

    minor bug fixes

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,13 @@ module.exports = {
   roots: ["<rootDir>/src/components"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.(js|jsx|mjs)$": "<rootDir>/node_modules/babel-jest"
+    "^.+\\.(js|jsx|mjs)$": "<rootDir>/node_modules/babel-jest",
+    "\\.(scss|sass|css)$": "identity-obj-proxy"
   },
   testRegex: "/__tests__/",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules"
+  },
   setupFilesAfterEnv: ["<rootDir>/src/setupEnzyme.ts"]
 };
